0977b5c0e3da3c8353658ba0b39d4fd55944a0b1
Israel <israelireta@gmail.com>
2022-07-12 14:05:39 -0500

    migraciones de Israel

---
+++ database/migrations/2022_07_12_170550_create_idioma_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIdiomaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('idioma', function (Blueprint $table) {
+            $table->id('id_idioma');
+            $table->boolean('idioma_español');
+            $table->timestamps();
+            $table->SoftDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('idioma');
+    }
+}
