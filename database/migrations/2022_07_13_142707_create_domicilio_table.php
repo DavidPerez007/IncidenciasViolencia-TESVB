e953f302256218fd3dde8db12f449ddaac6ce2ef
Israel <israelireta@gmail.com>
2022-07-13 11:14:49 -0500

    migraciones Isra

---
+++ database/migrations/2022_07_13_142707_create_domicilio_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDomicilioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domicilio', function (Blueprint $table) {
+            $table->id('id_domicilio');
+            $table->string('calle',50);
+            $table->string('no_exterior',10);
+            $table->string('no_interior',10);
+            $table->string('colonia',45);
+            $table->integer('cod_postal');
+            $table->unsignedBigInteger('id_municipio');
+            $table->foreign('id_municipio')->references('id_municipio')->on('municipio');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domicilio');
+    }
+}
