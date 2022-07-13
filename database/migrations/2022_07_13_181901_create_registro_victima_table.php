551a1a67c43c815195cb63e36eda299017a3e57b
Israel <israelireta@gmail.com>
2022-07-13 13:43:51 -0500

    migraciones Israel_4

---
+++ database/migrations/2022_07_13_181901_create_registro_victima_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistroVictimaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registro_victima', function (Blueprint $table) {
+            $table->id('id_registro_victima');
+            $table->unsignedBigInteger('id_datos_generales');
+            $table->foreign('id_datos_generales')->references('id_datos_generales')->on('datos_generales');
+            $table->string('vive_con',20);
+            $table->unsignedBigInteger('id_municipio');
+            $table->foreign('id_municipio')->references('id_municipio')->on('municipio');
+            $table->string('curp',18);
+            $table->unsignedBigInteger('id_domicilio');
+            $table->foreign('id_domicilio')->references('id_domicilio')->on('domicilio');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registro_victima');
+    }
+}
