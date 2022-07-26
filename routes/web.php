190df6595877b8a746ae00a209f442e57fa8bddd
Harlock <iscprimerocesar@gmail.com>
2022-07-26 09:53:22 -0500

    install livewire

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    //return view('template.test');
     return view('welcome');
 });
 Route::get('pagina_prueba',function(){
     return view('template.test');
+});
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
