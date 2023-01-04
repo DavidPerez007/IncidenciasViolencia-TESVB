3034b340ac9ee55410603299dfacf7b72f470839
Federico <fedeclim2000@gmail.com>
2023-01-04 15:48:47 -0600

    Cambios Fede 04/01/2022

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,7 @@
            <div class="text-center"><label class="text-center"><h2>Dashboard</h2></label></div>
 
            <div class="card">
+
                <div class="card-body">
 
                    <table id="tablaCatalogos1" class="table table-bordered table-hover table-striped  ">
@@ -53,11 +54,17 @@
 
                            </tr>
                        @endforeach
+
                        </tbody>
                    </table>
 
-
                </div>
+               {{--}}<x-maps-leaflet :centerPoint="['lat' => 19.2128167, 'long' =>  -100.1045106]"></x-maps-leaflet>
+
+        --}}
+
+               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25239.75273940181!2d-100.1458926943175!3d19.18213820266953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cd63813218f41f%3A0xb687c3a1fb52897c!2sValle%20de%20Bravo%2C%20M%C3%A9x.!5e1!3m2!1ses-419!2smx!4v1672867796716!5m2!1ses-419!2smx" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
            </div>
        </div>
 @endsection
