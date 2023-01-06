65069c0ee25792135816b7168af5e7535041e128
Federico <fedeclim2000@gmail.com>
2023-01-05 22:49:33 -0600

    Cambios Fede 05/01/2023

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
                             <div class="sb-sidenav-menu-heading">Core</div>
                             <a class="nav-link" href="{{url("dashboard")}}">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
+                                Estadística de Incidencias
                             </a>
                             <div class="sb-sidenav-menu-heading">Interface</div>
                             <a class="nav-link" href="{{url("datos_generales ")}}">
